d03f1e1d9bcfe2418963c81fcd8059fdd847d695
Robin <grambrobin@gmail.com>
2024-01-31 00:46:14 +0100

    Text cleanup and chances that can be changed for the bullets

--- bullets.py
+++ bullets.py
@@ -18,9 +18,16 @@ def new_game(bullets):
     bullets_list.append(0)  # Add a blank bullet
     bullets_list.append(1)  # Add a life bullet
 
-    # Fill the rest of the bullets randomly
-    for i in range(bullets - 3):  # Subtract 3 because we've already added 2 bullets and we'll add the endgame bullet later
-        bullets_list.append(random.randint(0, 1))
+    # Calculate the number of remaining bullets
+    remaining_bullets = bullets - 3  # Subtract 3 because we've already added 2 bullets and we'll add the endgame bullet later
+
+    # Calculate the number of blanks and lives based on the desired percentages
+    blanks = int(remaining_bullets * random.uniform(0.4, 0.6))  # 20-50% blanks
+    lives = remaining_bullets - blanks
+
+    # Add the blanks and lives to the bullet list
+    bullets_list.extend([0] * blanks)
+    bullets_list.extend([1] * lives)
 
     random.shuffle(bullets_list)  # Shuffle the bullets to randomize their order
 
@@ -29,8 +36,6 @@ def new_game(bullets):
 
     consistor.set_bullets(bullets_list)
     # Number of blanks
-    blanks = bullets_list.count(0)
-    consistor.set_blanks(blanks)
+    consistor.set_blanks(bullets_list.count(0))
     # Number of lives
-    lives = bullets_list.count(1)
-    consistor.set_lives(lives)
+    consistor.set_lives(bullets_list.count(1))
--- main.py
+++ main.py
@@ -19,12 +19,18 @@ def print_top_left(text):
 def print_top_right(text):
     columns = shutil.get_terminal_size().columns
     print(text.rjust(columns))
+    
+def print_top_left_and_right(left_text, right_text):
+    columns = shutil.get_terminal_size().columns
+    print(left_text.ljust(columns // 2) + right_text.rjust(columns // 2))
 
 def main():
-    print_centered("A Buckshot Roulette CLI Game")
+    clear_console()
+    print_centered("A Buckshot Roulette CLI Game by")
     print_centered("by")
     print_centered("NurRobin")
-    print_centered("Based on the game 'Buckshot Roulette' by")
+    print_centered("Based on the game 'Buckshot Roulette'")
+    print_centered("by")
     print_centered("Mike Klubnika")
     print_centered("Press Enter to continue")
     input()
@@ -35,16 +41,15 @@ def main():
     
     # Game Loop
     while True:
-        clear_console()
+        bullets.new_game(random.randint(3, 7))
         consistor.set_lifes_player1(4)
         consistor.set_lifes_player2(4)
-        bullets.new_game(random.randint(3, 7))
-        print_top_left("Player 1")
-        print_top_right("Player 2")
-        print_top_left("Lifes: " + str(consistor.get_lifes_player1()))
-        print_top_right("Lifes: " + str(consistor.get_lifes_player2()))
+        clear_console()
+        print_top_left_and_right("Player 1", "Player 2")
+        print_top_left_and_right("Lifes: " + str(consistor.get_lifes_player1()), "Lifes: " + str(consistor.get_lifes_player2()))
+        print("\n")
         # Print how many bullets are in the gun and how many are blanks and how many are lives
-        print_centered("There are " + str(consistor.get_blanks()) + " blanks and " + str(consistor.get_lives()) + " lives in the gun.")
+        print_centered("There are " + str(consistor.get_blanks()) + " blanks and " + str(consistor.get_lives()) + " lives.")
         print_centered("Press Enter to continue")
         input()
 
